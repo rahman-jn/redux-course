7cbdebfb72ba38911ebef53399b28516445d7014
git <git@git-client.com>
2022-01-28 08:41:04 +0330

    Thunc was added to project through getdefaultMiddleWare() method of reduxtoolkit. with this method the middleware calls action method if the action be a function and not an object.(By default if action type not be an object we'll get the error.)

--- src/index.js
+++ src/index.js
@@ -4,38 +4,6 @@ import * as projectsActions from './store/projects';
 import { addUser } from './store/users';
 
 const store = configureStore();
-
-const unsubscribe = store.subscribe( () => {
-    console.log("Store changed", store.getState());
-})
-
- store.dispatch(projectsActions.projectAdd({ name : "Project1 "}));
-// store.dispatch(projectsActions.projectAdd({ name : "Project2 "}));
-
-//store.dispatch(bugsActions.bugAdded({ description : "BUG1" } ));
-
-//Add new user to user slice
-store.dispatch(addUser({ name : "Rhman" }));
-
-// store.dispatch(bugsActions.bugAdded({ description : "BUG4" } ));
-// store.dispatch(bugsActions.bugAdded({ description : "BUG5" } ));
-// store.dispatch(bugsActions.bugAdded({ description : "BUG6" } ));
-
-//store.dispatch(bugsActions.bugAssignedToUser({ bugId : 1, userId : 1 }));
-
-
-
-//console.log(bugsActions.unResolvedBugs(store.getState()))
-// console.log(bugsActions.personBugs(1)(store.getState()));
-
-// store.dispatch(actions.bugAdded({ description : "BUG" }));
-// store.dispatch(actions.bugAdded({ description : "BUG3" }));
-
-//store.dispatch(actions.bugResolved({id : 1 }));
-
-//unsubscribe();
-
-//store.dispatch(actions.bugRemoved(1));
-
-
-//console.log(store.getState());
+store.dispatch(() => {
+    store.dispatch({ type : 'BugsReceived', bugs : [1,2,3,4]})
+});
--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,8 +1,9 @@
-const logger = store => next => action => (
-    console.log("Store", store),
-    console.log("Next", next),
-    console.log("Action", action),
+const logger = param => store => next => action => {
+    console.log("Logging", param);
+    // console.log("Store", store);
+    // console.log("Next", next);
+    console.log("Action", action);
     next(action)
-);
+};
 
 export default logger;
